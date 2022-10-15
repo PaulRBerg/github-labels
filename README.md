41ff5431ef3b22d2dd21ff66475bd406f6b14f4c
Paul Razvan Berg <hello@paulrberg.com>
2022-10-15 19:53:14 +0300

    Add priority labels
    Add instructions for how to apply priority labels
    Use "yml" extension instead of "yaml"

--- README.md
+++ README.md
@@ -2,18 +2,42 @@
 
 Useful for synchronizing labels across GitHub repositories. See [github-label-sync](https://github.com/Financial-Times/github-label-sync/).
 
+## Pre Requisites
+
+Install the following package with npm:
+
+```sh
+npm install -g github-label-sync
+```
+
+And make sure that you have your GitHub token set in the environment variable `GITHUB_TOKEN`.
+
 ## Usage
 
-Make sure that you have a GitHub token stored in the environment variable `GITHUB_TOKEN`.
+### Basic Labels
 
-To apply to all repos:
+To apply the basic labels to all repos:
 
 ```sh
 $ ./apply.s
 ```
 
-To apply to one particular repo:
+To apply the basic labels to one particular repo:
 
 ```sh
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml owner/repo
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./priorities.yml owner/repo
+```
+
+### Priority Labels
+
+To apply the priority labels to all repos:
+
+```sh
+$ ./apply.s
+```
+
+To apply the priority labels to one particular repo:
+
+```sh
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./priorities.yml owner/repo
 ```
--- apply.sh
+++ apply.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env zsh
 
 # Personal
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/create-eth-app
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/evm-bn
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/hardhat-packager
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/javascript-template
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/multisol
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/prb-contracts
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/prb-math
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/prb-proxy
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/solidity-template
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml paulrberg/typescript-template
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/create-eth-app
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/evm-bn
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/hardhat-packager
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/javascript-template
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/multisol
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/prb-contracts
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/prb-math
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/prb-proxy
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/solidity-template
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml paulrberg/typescript-template
 
 # Sablier
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/docs
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/frontend
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/safe-app
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/subgraph
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/v2-core
-github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yaml sablierhq/v2-interfaces
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/docs
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/frontend
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/safe-app
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/subgraph
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/v2-core
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml sablierhq/v2-interfaces
