236a1c418e737e0eac23d33b6c414b9d316019d9
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-05-21 22:53:12 +0300

    refactor: update GitHub usernames

--- apply.sh
+++ apply.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env zsh
 
 # Personal
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/create-eth-app
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/evm-bn
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/foundry-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/hardhat-packager
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/hardhat-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/javascript-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/multisol
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/prb-contracts
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/prb-math
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/prb-proxy
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/prb-test
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml paulrberg/typescript-template
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/create-eth-app
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/evm-bn
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/foundry-template
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/hardhat-packager
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/hardhat-template
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/javascript-template
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/multisol
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-contracts
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-math
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-proxy
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-test
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/typescript-template
 
 # Sablier
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/docs
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/safe-app
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/subgraph
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/v1-interfaces
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/v2-core
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/v2-interfaces
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablierhq/v2-periphery
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/docs
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/safe-app
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/subgraph
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v1-interfaces
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-core
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-interfaces
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-periphery
