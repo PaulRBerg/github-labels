18825eee5fb687d835b57c5eacba3c31e92ba8af
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-08-23 10:34:45 +0300

    feat: add sablier-labs/v2-sandbox

--- apply.sh
+++ apply.sh
@@ -29,4 +29,5 @@ github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./l
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-docs
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-interfaces
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-periphery
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-sandbox
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-subgraphs
