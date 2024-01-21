0142b58d3b4f0c70b58b844d007c548ba7f9a647
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2024-01-21 20:05:31 +0200

    Add cryptofolio-scripts repo

--- apply.sh
+++ apply.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 
 # Personal
+github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/cryptfolio-scripts
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/evm-bn
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/foundry-multibuild
 github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/foundry-template
