21d2164b759fc3465395334d1cd1b55c99b9e164
Paul Razvan Berg <prberg@proton.me>
2025-03-28 18:20:12 +0200

    refactor: make script more readable
    chore: add EditorConfig
    chore: add Prettier config
    docs: more instructions in README
    refactor: delete Sablier content

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # GitHub Labels
 
-Useful for synchronizing labels across GitHub repositories. See [github-label-sync](https://github.com/Financial-Times/github-label-sync/).
+Useful for synchronizing labels across GitHub repositories. See
+[github-label-sync](https://github.com/Financial-Times/github-label-sync/).
 
 ## Pre Requisites
 
 Install the following package with npm:
 
 ```sh
-npm install -g github-label-sync
+npm install --global github-label-sync
 ```
 
-And make sure that you have your GitHub token set in the environment variable `GITHUB_TOKEN`.
+And have your GitHub token set in the environment variable `GITHUB_TOKEN`. The token must have the `repo` and
+`public_repo` scopes.
 
 ## Usage
 
@@ -28,4 +30,13 @@ github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./l
 
 Running these scripts will NOT overwrite any existing labels in the repo.
 
-If you want to overwrite the labels, and also delete the labels that are not listed in the YAML files, remove the `--allow-added-labels` flag.
+If you want to overwrite the labels, and also delete the labels that are not listed in the YAML files, remove the
+`--allow-added-labels` flag.
+
+```sh
+github-label-sync --access-token $GITHUB_TOKEN --labels ./labels.yml owner/repo
+```
+
+> [!WARNING]
+>
+> The last command will delete any labels that are not listed in the YAML files. Use with caution.
--- apply.sh
+++ apply.sh
@@ -1,42 +1,33 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
-# Personal
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/cryptfolio-scripts
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/evm-bn
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/foundry-multibuild
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/foundry-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/hardhat-packager
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/hardhat-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/javascript-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/multisol
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-contracts
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-longevity
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-math
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-proxy
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/prb-test
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/rust-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml PaulRBerg/typescript-template
+# Strict mode: https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
+set -euo pipefail
 
-# Sablier
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/community-token-list
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/deploy-multi-chain
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/examples
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/flow
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/multi-gitter-scripts
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/policies
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/sablier-labs.github.io
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/sablier-v2-integration-template
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/sabvm
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/SRFs
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v1-subgraph
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v1-interfaces
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v1-protocol
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v1-safe-app
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-core
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-deployments
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-docs
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-interfaces
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-open-ended
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-periphery
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-sandbox
-github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml sablier-labs/v2-subgraphs
+# Function to reuse code
+sync_labels() {
+  github-label-sync --access-token $GITHUB_TOKEN --allow-added-labels --labels ./labels.yml "$1"
+}
+
+# Personal repositories
+REPOS=(
+  "PaulRBerg/cryptfolio-scripts"
+  "PaulRBerg/evm-bn"
+  "PaulRBerg/foundry-multibuild"
+  "PaulRBerg/foundry-template"
+  "PaulRBerg/hardhat-packager"
+  "PaulRBerg/hardhat-template"
+  "PaulRBerg/javascript-template"
+  "PaulRBerg/multisol"
+  "PaulRBerg/prb-contracts"
+  "PaulRBerg/prb-longevity"
+  "PaulRBerg/prb-math"
+  "PaulRBerg/prb-proxy"
+  "PaulRBerg/prb-test"
+  "PaulRBerg/rust-template"
+  "PaulRBerg/typescript-template"
+)
+
+# Loop through and sync each repository
+for repo in "${REPOS[@]}"; do
+  sync_labels "$repo"
+done
